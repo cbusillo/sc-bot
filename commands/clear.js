b400064f2b368a21542e9b86092945e161ac0896
Chris Busillo <info@shinycomputers.com>
2021-10-15 14:44:46 -0400

    fix clear

--- commands/clear.js
+++ commands/clear.js
@@ -2,10 +2,7 @@ module.exports.run = async (bot, message, args) => {
     
         let fetched;
         do {
-          fetched = await bot.channels.cache.find(c => c.name === "bot-testing").messages.fetch({limit: 100})
-          
-          //message.channel.messages.fetch({limit: 100});
-          
+          fetched = await bot.channels.cache.find(c => c.name === "bot-testing").messages.fetch({limit: 10})
           message.channel.bulkDelete(fetched);
         }
         while(fetched.size >= 2);
